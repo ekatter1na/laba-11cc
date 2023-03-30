b14bbe3ff6cc6f6e824654cbc7d50d3525b000ac
ekatter1na <98614159+ekatter1na@users.noreply.github.com>
2023-03-30 10:28:56 +0300

    Add files via upload

---
+++ laba 11cc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Laba_11_cc
+{
+    internal class Program
+    {
+        delegate string WeekDaysDelegate();
+        static void Main(string[] args)
+        {
+            string[] WeekDays = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+            int index = -1;
+
+            WeekDaysDelegate weekDaysDelegate = () =>
+            {
+                index = (index + 1) % 7;
+                return WeekDays[index];
+            };
+
+            for (int i = 0; i < 17; i++)
+            {
+                Console.WriteLine(weekDaysDelegate());
+            }
+        }
+    }
+}
